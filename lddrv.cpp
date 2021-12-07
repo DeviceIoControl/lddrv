cd783a8ae2909361d4c5be728a25b107b2e85aeb
DeviceIoControl <joshsteph@hotmail.co.uk>
2021-12-07 18:50:28 +0000

    Fixed argument parser and stabilised driver unloading and loading.

--- lddrv.cpp
+++ lddrv.cpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <unordered_map>
 #include <winternl.h>
+#include <processthreadsapi.h>
 
 #include  "..\SvcManager\SvcManager.hpp"
 
@@ -19,11 +20,12 @@ public:
     static bool Initialise() 
     {
         HMODULE hNTDLL = GetModuleHandleA("ntdll.dll");
+
         DriverManager::s_pfnNtLoadDriver = reinterpret_cast<T_NtLoadDriver>(GetProcAddress(hNTDLL, "NtLoadDriver"));
         DriverManager::s_pfnNtUnloadDriver = reinterpret_cast<T_NtUnloadDriver>(GetProcAddress(hNTDLL, "NtUnloadDriver"));
 
         std::cout << "Obtaining required privileges...\n";
-        DriverManager::GetLoadDriverPrivilege();
+        return DriverManager::GetLoadDriverPrivilege();
     }
 
     static bool LoadDriver(const std::wstring& DriverServiceName) 
@@ -59,94 +61,146 @@ private:
     inline static T_NtLoadDriver s_pfnNtLoadDriver;
     inline static T_NtUnloadDriver s_pfnNtUnloadDriver;
 
-    static bool GetLoadDriverPrivilege() 
+    static HANDLE GetProcessToken() 
     {
         bool Success = true;
         HANDLE hProcessToken = NULL;
-        OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &hProcessToken);
-        
-        DWORD dwReturnLength = NULL;
 
+        if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &hProcessToken)) 
+        {
+            std::cout << "Cannot open process token!\n";
+            return INVALID_HANDLE_VALUE;
+        }
+
+        return hProcessToken;
+    }
+
+    static TOKEN_PRIVILEGES GetTokenPrivilegeFromName(const std::string& szPrivName)
+    {
         TOKEN_PRIVILEGES NewTknPrivs = { 0 };
         NewTknPrivs.PrivilegeCount = 1;
         NewTknPrivs.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-        LookupPrivilegeValueA(nullptr, "SeLoadDriverPrivilege", &NewTknPrivs.Privileges[0].Luid);
 
-        if (AdjustTokenPrivileges(hProcessToken, false, &NewTknPrivs, NULL, nullptr, &dwReturnLength))
+        if (!LookupPrivilegeValueA(nullptr, szPrivName.c_str(), &NewTknPrivs.Privileges[0].Luid))
         {
-            std::unique_ptr<TOKEN_PRIVILEGES> pOldTknPrivs = std::make_unique<TOKEN_PRIVILEGES>();
-            if (!AdjustTokenPrivileges(hProcessToken, false, &NewTknPrivs, dwReturnLength, pOldTknPrivs.get(), &dwReturnLength))
-            {
-                std::cout << "Unable to gain SeLoadDriverPrivilege.\n";
-                Success = false;
-            }
+            std::cout << "Unable to gain SeLoadDriverPrivilege.\n";
         }
+
+        return NewTknPrivs;
+    }
+
+    static bool ApplyTokenPrivilege(HANDLE hProcessToken, const TOKEN_PRIVILEGES& tokenPriv)
+    {
+        DWORD dwReturnLength = NULL;
+        TOKEN_PRIVILEGES NewTokenPrivs = tokenPriv;
+       
+        if(!AdjustTokenPrivileges(hProcessToken, false, &NewTokenPrivs, NULL, nullptr, &dwReturnLength))
+        {
+            std::cout << "Unable to apply privileges to the process access token.\n";
+            return false;
+        }
+        
+        return true;
+    }
+
+    static bool GetLoadDriverPrivilege() 
+    {
+        HANDLE hProcessToken = DriverManager::GetProcessToken();
+        TOKEN_PRIVILEGES tokenPriv = DriverManager::GetTokenPrivilegeFromName("SeLoadDriverPrivilege");
+        bool Success = DriverManager::ApplyTokenPrivilege(hProcessToken, tokenPriv);
 
         CloseHandle(hProcessToken);
         return Success;
     }
-
 };
 
 int main(int argc, const char** argv)
 {
-    std::cout << "lddrv - Load Driver command-line utility\n";
-    std::cout << "Created by Josh S.\n\n";
-
-    std::cout << "Creating driver service...\n";
-    
-    ServiceManager::Initialize();
-    DriverManager::Shutdown();
+    std::cout << "lddrv - Load Driver command-line utility [Version 0.9.9a]\n";
+    std::cout << "(c) Created by Josh S. All rights reserved.\n\n";
 
     if (argc < 5) 
     {
         std::cout << "Insufficient arguments provided.\n";
-        ServiceManager::Shutdown();
         return ERROR_INVALID_PARAMETER;
     }
 
-    std::unordered_map<std::string_view, std::string_view> ArgumentMap;
-    ArgumentMap[std::string_view("-binpath")] = std::string_view();
-    ArgumentMap[std::string_view("-svcname")] = std::string_view();
-    ArgumentMap[std::string_view("-operation")] = std::string_view();
+    ServiceManager::Initialise();
+    DriverManager::Initialise();
 
+    std::unordered_map<std::string_view, const char*> ArgumentMap;
+    ArgumentMap.emplace("-binpath", nullptr);
+    ArgumentMap.emplace("-svcname", nullptr);
+    ArgumentMap.emplace("-operation", nullptr);
+
+    // Get the parameter for each argument provided and assign it to it's corresponding argument in the Argument map.
     for (int i = 1; i < argc; ++i) 
     {
-        if (ArgumentMap.find(argv[i]) == ArgumentMap.end()) 
+        auto argumentItrPos = ArgumentMap.find(argv[i]);
+
+        // Ensure the argument is present in the arg map, and verify the parameter to an arg is not an arg. 
+        // (E.g "-binpath -operation create" will not work.)
+        if (argumentItrPos == ArgumentMap.end() || argv[i + 1][0] == '-')
         {
             std::cout << "Invalid argument was provided: " << argv[i] << "\n";
+            
+            DriverManager::Shutdown();
             ServiceManager::Shutdown();
+            
             return ERROR_INVALID_PARAMETER;
         }
 
-        //Get the parameter for each argument provided and assign it to each of the arguments in Argument map.
-        ArgumentMap[argv[i++]] = ArgumentMap[argv[i + 1]];
+        argumentItrPos->second = argv[++i];
     }
 
-    const std::string_view& svcName = ArgumentMap[std::string_view("-svcname")];
-    const std::string_view& binPath = ArgumentMap[std::string_view("-binpath")];
+    const std::string_view svcName = ArgumentMap[std::string_view("-svcname")];
+    const std::string_view binPath = ArgumentMap[std::string_view("-binpath")];
+    const std::string_view operation = ArgumentMap[std::string_view("-operation")];
 
-    ServiceHandle hDrvService = ServiceManager::CreateService(svcName.data(), svcName.data(),
-        SVC_TYPE::KERNEL_DRIVER, SVC_START_TYPE::MANUAL, SVC_ERROR_CTRL::ERROR_NORMAL, 
-        binPath.data());
+    ServiceHandle hDriverService;
+    std::unique_ptr<QUERY_SERVICE_CONFIGA> svcConfig = hDriverService.QueryConfig();
 
-    HANDLE hProcessToken = NULL;
-    std::cout << "Attempting to load driver...\n";
+    //Convert from narrow to wide-char string.
+    std::wstring DriverSvcName(svcName.begin(), svcName.end());
     
-    //Convert from ASCII to wide-char string.
-    std::wstring DriverName(svcName.begin(), svcName.end());
-    DriverManager::LoadDriver(DriverName);
+    if (operation == "create") 
+    {
+        std::cout << "Creating driver service...\n";
+        hDriverService = ServiceManager::CreateService(svcName.data(), svcName.data(),
+            SVC_TYPE::KERNEL_DRIVER, SVC_START_TYPE::MANUAL, SVC_ERROR_CTRL::ERROR_NORMAL,
+            binPath.data());
 
-
-
-    
-    DriverManager::UnloadDriver(DriverName);
-    ServiceManager::DeleteService(hDrvService);
-    
+        std::cout << "Attempting to load driver...\n";    
+        if (DriverManager::LoadDriver(DriverSvcName)) 
+        {
+            std::cout << "Driver was loaded successfully!\n";
+        }
+        else 
+        {
+            std::cout << "Failed to load driver.\n";
+        }
+    }
+    else if (operation == "delete") 
+    {
+        hDriverService = ServiceManager::OpenService(svcName.data(), SVC_ACCESS::ALL_ACCESS);
+        
+        if (svcConfig->dwServiceType != (DWORD)SVC_TYPE::KERNEL_DRIVER)
+        {
+            std::cout << "Unable to remove the driver service (Invalid service type).\n";
+        }
+        else
+        {
+            std::cout << "Unloading driver...\n";
+            DriverManager::UnloadDriver(DriverSvcName);
+            
+            std::cout << "Removing driver service...\n";
+            ServiceManager::DeleteService(hDriverService);
+        }
+    }
+ 
     ServiceManager::Shutdown();
     DriverManager::Shutdown();
 
-    std::getchar();
     return ERROR_SUCCESS;
 }
 
